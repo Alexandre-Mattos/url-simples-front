d4de5ac31d13672e285a204c3f668643411ce177
Alexandre-Mattos <alexandremattosbc@gmail.com>
2024-04-15 18:41:36 -0300

    [CHANGE] Change the redirect method

--- pages/[id].vue
+++ pages/[id].vue
@@ -2,13 +2,15 @@
 
 <script>
 export default {
-  created() {
-    this.redirect()
+  async created() {
+    await this.redirect()
   },
 
   methods: {
     async redirect() {
-      window.location.href = `${this.$config.app.backendUrl}/${this.$route.params.id}`
+      this.$router.push({
+        path: `${this.$config.app.backendUrl}/${this.$route.params.id}`,
+      })
     },
   },
 }
