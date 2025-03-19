3e8070960fcb75ec482e9a6b20deb2a7c59a2a28
Alexandre Mattos <alexandremattosbc@gmail.com>
2025-03-19 13:42:27 -0300

    remove

--- app.vue
+++ app.vue
@@ -6,18 +6,3 @@
   </NuxtLayout>
 </template>
 
-<script setup>
-import { useHead, useRuntimeConfig } from "#imports";
-
-const config = useRuntimeConfig();
-
-useHead({
-  script: [
-    {
-      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
-      async: true,
-      "data-ad-client": config.app.adsenseClient, 
-    },
-  ],
-});
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,9 @@ export default defineNuxtConfig({
       script: [
         {
           hid: "adsbygoogle",
-          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
-          "data-ad-client": process.env.ADSENSE_CLIENT_ID,
+          src: `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.ADSENSE_CLIENT_ID}
+     crossorigin="anonymous`,
           async: true,
-          crossorigin: "anonymous",
         },
       ],
     },
